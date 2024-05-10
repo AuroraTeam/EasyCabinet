be6e027a3c1978d3041410f5d6a0ebd90cbaddf6
Andrew Molchanov <mr.catmanrus@gmail.com>
2024-05-10 22:44:42 +0300

    Добавил главную страницу

--- packages/frontend/src/pages/index.jsx
+++ packages/frontend/src/pages/index.jsx
@@ -1,21 +1,41 @@
-import { useNavigate } from "@solidjs/router";
-import { isAuthed, isLoaded } from "../api";
-import { createEffect } from "solid-js";
+import { FaBrandsWindows, FaBrandsLinux, FaBrandsApple } from "solid-icons/fa";
 
 export default function Index() {
-  const navigate = useNavigate();
+  return (
+    <div className="h-full flex flex-col items-center justify-center">
+      <h1 className="text-4xl text-center text-green-100">
+        Добро пожаловать на Project Name
+      </h1>
 
-  createEffect(() => {
-    if (!isLoaded()) {
-      return;
-    }
-
-    if (!isAuthed()) {
-      navigate("/login", { replace: true });
-    } else {
-      navigate("/profile", { replace: true });
-    }
-  });
-
-  return <>Loading...</>;
+      <h2 className="mt-4 text-2xl text-center text-green-100">
+        Скачать лаунчер:
+      </h2>
+      <div className="mt-4 flex gap-4 items-center justify-center text-center">
+        <a
+          className="bg-neutral-700 hover:shadow-xl hover:shadow-neutral-900 hover:bg-neutral-600 hover:text-green-100 rounded-md px-4 py-2"
+          download
+          href="#"
+        >
+          <FaBrandsWindows class="w-32 h-32 p-4" />
+          Windows
+        </a>
+        <a
+          className="bg-neutral-700 hover:shadow-xl hover:shadow-neutral-900 hover:bg-neutral-600 hover:text-green-100 rounded-md px-4 py-2"
+          download
+          href="#"
+        >
+          <FaBrandsLinux class="w-32 h-32 p-4" />
+          Linux
+        </a>
+        <a
+          className="bg-neutral-700 hover:shadow-xl hover:shadow-neutral-900 hover:bg-neutral-600 hover:text-green-100 rounded-md px-4 py-2"
+          download
+          href="#"
+        >
+          <FaBrandsApple class="w-32 h-32 p-4" />
+          Macos
+        </a>
+      </div>
+    </div>
+  );
 }
